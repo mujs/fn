a05d717bd8ebc59d87f7e073525e6ec7f2547b13
Pau Fracés <pfraces@gmail.com>
2014-09-16 21:55:11 +0200

    Update fn.js

--- fn.js
+++ fn.js
@@ -1,18 +1,23 @@
 define('mu.fn', function () {
   'use strict';
   
+  var apply = function (fn, argv) {
+    return fn.apply(null, argv);
+  };
+  
   var partial = function () {
     var argv = [].slice.call(arguments),
         fn = argv.shift();
         
-    var bound = function () {
+    var partialApplied = function () {
       return fn.apply(null, [].concat.apply(argv, arguments));
     };
     
-    return bound;
+    return partialApplied;
   };
   
   return {
+    apply: apply,
     partial: partial
   };
 });
